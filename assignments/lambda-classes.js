744b57ecf64207e3b22a310c5a49fea9c116bfcf
Daisy Mesa <Daisy@DMM.fios-router.home>
2019-04-04 18:02:52 -0400

    In lambda-classes file, created Person class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,17 @@
 // CODE here for your Lambda Classes
+
+// ****************** PERSON ******************
+class Person {
+    constructor (properties) {
+      this.name = properties.name;
+      this.age = properties.age;
+      this.location = properties.location;
+      this.gender = properties.gender;
+
+    }
+    speak(){
+      return `Hello, my name is ${this.name}, I am from ${this.location}.`;
+    }
+}
+
+
