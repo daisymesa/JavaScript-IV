4f501b7a123679098b0a5bad92a6f2f643d991ab
Daisy Mesa <Daisy@DMM.fios-router.home>
2019-04-05 00:40:06 -0400

    Added test objects to lambda-classes file

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -22,7 +22,6 @@ class Instructor extends Person {
         this.specialty = instructorProperties.specialty;
         this.favLanguage = instructorProperties.favLanguage;
         this.catchPhrase = instructorProperties.catchPhrase;
-
     }
     demo(subject) {
           return `Today we are learning about ${subject}.`;
@@ -39,7 +38,6 @@ class Student extends Person {
         this.previousBackground = studentProperties.previousBackground;
         this.className = studentProperties.className;
         this.faveSubjects = studentProperties.faveSubjects;
-
     }
     listsSubjects() {
         return this.faveSubjects;
@@ -58,7 +56,6 @@ class ProjectManager extends Instructor {
         super(projectManagerProperties);
         this.gradClassName = projectManagerProperties.gradClassName;
         this.faveInstructor = projectManagerProperties.faveInstructor;
-
     }
     standUp(channel) {
           return `${this.name} announces to ${channel}, @channel standy times!​​​​​`;
@@ -67,3 +64,99 @@ class ProjectManager extends Instructor {
         return `${this.name} debugs ${student}'s code on ${subject}.`;
     }
 }
+
+const david = new Instructor ({
+    name: "David",
+    location: "New York",
+    age: 35,
+    gender: "male",
+    favLanguage: "Python",
+    speciality: "Computer Science",
+    catchPhrase:"Don't forget the homies"
+})
+
+const annie = new Instructor ({
+    name: "Annie",
+    location: "Philadelphia",
+    age: 32,
+    gender: "female",
+    favLanguage: "JavaScript",
+    speciality: "Front-End",
+    catchPhrase:"I love cats and JS"
+})
+
+const oscar = new Instructor ({
+    name: "Oscar",
+    location: "New Jersey",
+    age: 39,
+    gender: "male",
+    favLanguage: "C++",
+    speciality: "Back-End",
+    catchPhrase:"You can do this"
+})
+
+const albert = new Student ({
+    name: "Albert",
+    location: "Orlando",
+    age: 27,
+    gender: "male",
+    previousBackground: "Chef",
+    gradClassName: "Web19",
+    faveSubjects: ["CSS", "LESS"]
+})
+
+const sara = new Student ({
+    name: "Sara",
+    location: "Boston",
+    age: 29,
+    gender: "female",
+    previousBackground: "Teacher",
+    gradClassName: "Web20",
+    faveSubjects: ["CSS", "HTML"]
+})
+
+const sebastian = new Student ({
+    name: "Sebastian",
+    location: "San Diego",
+    age: 21,
+    gender: "male",
+    previousBackground: "Physical Trainer",
+    gradClassName: "Web20",
+    faveSubjects: ["JavaScript", "React"]
+})
+
+const esther = new ProjectManager ({
+    name: "Esther",
+    location: "Chicago",
+    age: 36,
+    gender: "female",
+    favLanguage: "CSS",
+    speciality: "Front-End",
+    catchPhrase:"I know how you feel",
+    gradClassName: "Web17",
+    faveInstructor: "Oscar"
+})
+
+const luisa = new ProjectManager ({
+    name: "Luisa",
+    location: "Houston",
+    age: 28,
+    gender: "female",
+    favLanguage: "JavaScript",
+    speciality: "Front-End",
+    catchPhrase:"You got this",
+    gradClassName: "Web17",
+    faveInstructor: "Albert"
+})
+
+const julian = new ProjectManager ({
+    name: "Julian",
+    location: "Baltimore",
+    age: 27,
+    gender: "male",
+    favLanguage: "Java",
+    speciality: "Back-End",
+    catchPhrase:"I'm here to help'",
+    gradClassName: "Web17",
+    faveInstructor: "Annie"
+})
